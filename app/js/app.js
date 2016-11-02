00156b7d41afe40d25a34cb36576ebc460bd0708
Ufuk Bal <ufukbal@users.noreply.github.com>
2016-11-02 17:25:23 +0300

    Routing

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,10 @@
 angular.module('OverwatchStatsApp', [
   'OverwatchStatsApp.controllers',
-  'OverwatchStatsApp.services'
-]);
+  'OverwatchStatsApp.services',
+  'ngRoute'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+	when("/heroes", {templateUrl: "partials/heroes.html", controller: "heroesController"}).
+	otherwise({redirectTo: '/heroes'});
+}]);
